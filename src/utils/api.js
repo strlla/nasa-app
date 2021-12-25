8837d6003efadea97761830ce532ab1ab7a639d5
strlla <estrellaportocarrero@gmail.com>
2021-12-25 12:51:57 -0300

    Add params

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,9 @@
-import axios from 'axios'
-import { API_KEY } from '../config'
+import axios from "axios";
+import { API_KEY } from "../config";
 
-export const getMarsRoverImages = async (rover = 'curiosity') => {
- const response = await axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=1000&api_key=${API_KEY}`)
- return response;
-}
+export const getMarsRoverImages = async ({rover = "curiosity", sol = "1000"}) => {
+  const response = await axios.get(
+    `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&api_key=${API_KEY}`
+  );
+  return response;
+};
--- src/utils/useAxios.js
+++ src/utils/useAxios.js
@@ -1,23 +1,23 @@
-import React, {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 
-export const useAxios = (apiCall) => {
-    const [response, setResponse] = useState(null);
-    const [error, setError] = useState('');
-    const [loading, setloading] = useState(true);
+export const useAxios = (api, params) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setloading] = useState(true);
 
-    const fetchData = async () => {
-        try{
-            const response = await apiCall()
-            setResponse(response.data)
-            setloading(false)     
-        } catch(e) {
-            setError(e)
-        }
-    };
+  const fetchData = async () => {
+    try {
+      const response = await api(params);
+      setResponse(response.data);
+      setloading(false);
+    } catch (e) {
+      setError(e);
+    }
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    return { response, error, loading };
+  return { response, error, loading };
 };
