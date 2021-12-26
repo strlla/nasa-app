fc42333b194ff281fd164c2ee47fbe3d5cffa0e8
strlla <estrellaportocarrero@gmail.com>
2021-12-26 01:08:10 -0300

    Allow params

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,17 @@
 import axios from "axios";
 import { API_KEY } from "../config";
 
-export const getMarsRoverImages = async ({rover = "curiosity", sol = "1000"}) => {
+export const getMarsRoverImages = async ({
+  rover = "curiosity",
+  sol = "1000",
+  earthDate = "",
+}) => {
   const response = await axios.get(
-    `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&api_key=${API_KEY}`
+    `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos${
+      !earthDate ? `?sol=${sol}` : ""
+    }?earth_date=${earthDate.year}-${earthDate.month}-${
+      earthDate.day
+    }&api_key=${API_KEY}`
   );
   return response;
 };
